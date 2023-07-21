3fb8462f23a04fb42c98cb1a216c45d4961cd80d
Burak Sümer <buraksumer13@gmail.com>
2023-07-21 14:20:41 +0300

    Make svelte:head a component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,38 +1,13 @@
 <script>
 	import '../app.css';
 	import CTA from './CTA/CTA.svelte';
+	import Head from './head/Head.svelte';
 	import Navbar from './navbar/Navbar.svelte';
 	import Transition from './transition/transition.svelte';
 	export let data;
 </script>
 
-<svelte:head>
-	<!-- OG Meta -->
-	<meta name="title" content="Mercury Cafe Online Menü" />
-	<meta name="description" content="Mercury Cafe'nin ürünlerini içeren online menü." />
-	<meta
-		name="keywords"
-		content="Kahve, Çay, Mercury Cafe, Yiyecekler, Soğuk Meşrubatlar, Sıcak Kahveler, Soğuk Kahveler, Kokteyller"
-	/>
-	<meta name="robots" content="index, follow" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="Turkish" />
-	<title>Mercury Cafe Online Menü</title>
-
-	<!-- Open Graph / Facebook -->
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://mercury.mulayim.app" />
-	<meta property="og:title" content="Mercury Cafe  — Online Menü" />
-	<meta property="og:description" content="Mercury Cafe'nin ürünlerini içeren online menü." />
-	<meta property="og:image" content="%sveltekit.assets%/favicon.png" />
-
-	<!-- Twitter -->
-	<meta property="twitter:card" content="summary_large_image" />
-	<meta property="twitter:url" content="https://mercury.mulayim.app" />
-	<meta property="twitter:title" content="Mercury Cafe  — Online Menü" />
-	<meta property="twitter:description" content="Mercury Cafe'nin ürünlerini içeren online menü." />
-	<meta property="twitter:image" content="%sveltekit.assets%/favicon.png" />
-</svelte:head>
+<Head />
 
 <div class="min-h-screen overflow-hidden bg-gray-100 min-w-screen">
 	<Navbar />
