9b5c6092498bead5491df8c40a4c3666a2725c87
Burak Sümer <buraksumer13@gmail.com>
2023-07-26 11:36:19 +0300

    make loader sticky

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -7,7 +7,7 @@
 
 {#each collections as collection}
 	<div class="m-auto my-5">
-		<a href={collection.href} data-sveltekit-preload-code="eager" class="no-underline">
+		<a href={collection.href} class="no-underline">
 			<div class="m-auto overflow-hidden border rounded shadow-md max-w-screen">
 				<div class="px-6 py-4">
 					<div class="mb-2 text-xl font-bold">{collection.name}</div>
